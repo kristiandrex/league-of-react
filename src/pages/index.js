a604f21f384580a373f17bbee83c0cd92a6760c4
kristiandrex <cristiandrestorres@gmail.com>
2021-07-16 12:26:28 -0500

    Add search for champions

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import Champions from "@/components/Champions";
+
+import { useRouter } from "next/dist/client/router";
+import Search from "@/components/Search";
 
 export async function getStaticProps() {
   const { version, champions } = require("@/public/data/latest.json");
@@ -7,18 +11,45 @@ export async function getStaticProps() {
   return {
     props: {
       version,
-      champions
+      initialChampions: champions
     }
   };
 }
 
-function Home({ version, champions }) {
+function Home({ version, initialChampions }) {
+  const [champions, setChampions] = useState(initialChampions);
+  const router = useRouter();
+
+  useEffect(() => {
+    const search = router.query.search;
+
+    if (!search) {
+      return setChampions(initialChampions);
+    }
+
+    const filter = initialChampions.filter((champion) =>
+      champion.name.toLowerCase().startsWith(search)
+    );
+
+    setChampions(filter);
+  }, [router.query.search, initialChampions]);
+
+  const handleSearch = (value) => {
+    router.push({
+      pathname: "/",
+      query: {
+        search: value
+      }
+    });
+  };
+
   return (
     <>
       <Head>
         <title>League of React</title>
       </Head>
       <main>
+        <Search onChange={handleSearch} />
         <span className="version">Versión: {version}</span>
         <Champions champions={champions} />
       </main>
